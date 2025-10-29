9f2f07a585076ad00830f824dd21636a70b24702
catamv3 <michaelcatalanotti4401@gmail.com>
2025-10-29 15:57:13 -0400

    todo 2 and 3 complete.

--- app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/Screen3.kt
+++ app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/Screen3.kt
@@ -1,5 +1,6 @@
 package edu.farmingdale.pizzapartybottomnavbar
 
+import android.R
 import android.content.Intent
 import android.net.Uri
 import androidx.compose.foundation.background
@@ -23,39 +24,69 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.sync.Mutex
 
-// ToDo 2: the slider should be able to change the text value of the screen
-
-// ToDo 3: Make the UI look better by adding a gradient background (vertical) and padding
 
 @Composable
 fun Screen3() {
     var sliderValue by remember { mutableStateOf(0.5f) }
     var chkd by remember { mutableStateOf(true) }
+    //create gradient pattern
+    var gradient = Brush.verticalGradient(
+        colors = listOf(
+            Color(0xFFE3E5EC),
+            Color(0xFF87CCE8),
+            Color(0xFF3F76A1)
+        )
+    )
 
 
     val context = LocalContext.current
-    Column ( modifier = Modifier.padding(horizontal = 20.dp).fillMaxSize(),
+    // ToDo 3: Make the UI look better by adding a gradient background (vertical) and padding
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(brush = gradient)
+            .padding(24.dp),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally){
-        Slider(value = sliderValue, onValueChange = { sliderValue=it }, Modifier.fillMaxWidth()
-            , enabled = chkd)
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
-        Text (fontSize = 20.sp, text = "Second Screen" )
+        //ToDo 2: the slider should be able to change the text value of the screen
+        Slider(
+            value = sliderValue,
+            onValueChange = { sliderValue = it },
+            Modifier.fillMaxWidth(),
+            enabled = chkd
+        )
 
-        Button(onClick = { val newInt = Intent(Intent.ACTION_VIEW)
+        Text(
+            text = "Slide me to check my value: ${String.format("%.2f", sliderValue)}!",
+            fontSize = (sliderValue * 40).sp,  // font size from 0sp to 40sp
+            style = TextStyle(textAlign = TextAlign.Center),
+            color = Color.White
+        )
+
+        Button(onClick = {
+            val newInt = Intent(Intent.ACTION_VIEW)
             newInt.setData(Uri.parse("tel:6314202000"))
-            context.startActivity(newInt) }) {
-            Text(fontSize = 20.sp, text ="Call me")
+            context.startActivity(newInt)
+        }) {
+            Text(fontSize = 20.sp, text = "Call me")
         }
 
-        Checkbox(checked = chkd, onCheckedChange = { chkd=it }, modifier = Modifier.padding(10.dp))
+        Checkbox(
+            checked = chkd,
+            onCheckedChange = { chkd = it },
+            modifier = Modifier.padding(10.dp)
+        )
 
     }
 
 }
-
 
 
