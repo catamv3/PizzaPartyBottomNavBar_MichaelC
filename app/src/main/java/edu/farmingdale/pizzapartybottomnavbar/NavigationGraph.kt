4befd2743c3fbb88f1a8040ce5d1b79f74c8fffb
catamv3 <michaelcatalanotti4401@gmail.com>
2025-11-03 02:06:57 -0500

    updating color palette

--- app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/NavigationGraph.kt
+++ app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/NavigationGraph.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-
+//ToDo 9: updating color pallete
 // ToDo 8:
 @Composable
 fun NavigationGraph(
