0960a7f31b2091173b8636f88017f6c506605126
catamv3 <michaelcatalanotti4401@gmail.com>
2025-11-03 05:11:04 -0500

    commit

--- app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/ui/theme/Color.kt
+++ app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/ui/theme/Color.kt
@@ -3,17 +3,17 @@ package edu.farmingdale.pizzapartybottomnavbar.ui.theme
 import androidx.compose.ui.graphics.Color
 
 // ---------- Light Mode: Purples ----------
-val Mauve = Color(0xFFE0B0FF)          // Light, soft, delicate purple
-val Orchid = Color(0xFFDA70D6)         // Warm, flowery purple
-val RebeccaPurple = Color(0xFF663399)  // Balanced, classic purple
-val TruePurple = Color(0xFF800080)     // Standard base purple
-val DarkViolet = Color(0xFF9400D3)     // Deep, rich purple
-val Indigo = Color(0xFF4B0082)         // Cool, dark blue-purple
+val Mauve = Color(0xFFE0B0FF)
+val Orchid = Color(0xFFDA70D6)
+val RebeccaPurple = Color(0xFF663399)
+val TruePurple = Color(0xFF800080)
+val DarkViolet = Color(0xFF9400D3)
+val Indigo = Color(0xFF4B0082)
 
 // ---------- Dark Mode: Blues ----------
-val MidnightBlue = Color(0xFF191970)   // Deep night blue
-val NavyBlue = Color(0xFF000080)       // Standard navy
-val RoyalBlue = Color(0xFF4169E1)      // Bright royal blue
-val SteelBlue = Color(0xFF4682B4)      // Muted steel blue
-val SkyBlue = Color(0xFF87CEEB)        // Soft light blue highlight
-val BabyBlue = Color(0xFFB0E0E6)       // Pale blue accent
+val MidnightBlue = Color(0xFF191970)
+val NavyBlue = Color(0xFF000080)
+val RoyalBlue = Color(0xFF4169E1)
+val SteelBlue = Color(0xFF4682B4)
+val SkyBlue = Color(0xFF87CEEB)
+val BabyBlue = Color(0xFFB0E0E6)
